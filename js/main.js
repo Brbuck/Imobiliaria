ecee2f217b293e2f3fa03d858cc3bf6b3eaed499
Brbuck <62665590+Brbuck@users.noreply.github.com>
2021-04-29 13:49:17 -0300

    Update no Projeto

--- js/main.js
+++ js/main.js
@@ -1,25 +1,31 @@
-let slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+const botao = document.querySelector('.toggle')
+const menu = document.querySelector('nav')
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+botao.addEventListener('click', ()=>{
+  menu.classList.toggle('showMenu')
+})
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  
-  slides[slideIndex-1].style.display = "block";
- 
-}
+const nav_imovies = document.querySelector('.nav_imoveis')
+const menu_imovies = document.querySelector('.menu_imoveis')
+nav_imovies.addEventListener('click', ()=>{
+menu_imovies.classList.toggle('showImoveis')
+})
+
+const nav_Institucional = document.querySelector('.nav_Institucional')
+const menu_Institucional = document.querySelector('.menu_Institucional')
+nav_Institucional.addEventListener('click', ()=>{
+menu_Institucional.classList.toggle('showInstitucional')
+})
+
+const nav_Serviços = document.querySelector('.nav_Serviços')
+const menu_Serviços = document.querySelector('.menu_Serviços')
+nav_Serviços.addEventListener('click', ()=>{
+menu_Serviços.classList.toggle('showServiços')
+})
+
+const nav_Login = document.querySelector('.nav_Login')
+const menu_Login = document.querySelector('.menu_Login')
+nav_Login.addEventListener('click', ()=>{
+menu_Login.classList.toggle('showLogin')
+})
